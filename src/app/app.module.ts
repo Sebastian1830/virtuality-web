655f1bcf2802cb061fd3ed1ec9eb3276ebdf18b1
Sebastian Soto Varas <rsotov1@upao.edu.pe>
2021-01-18 08:47:56 -0500

    first commit v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TextFieldModule } from '@angular/cdk/text-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// COMPONENTS
+import { PatientListComponent } from './components/patient-list/patient-list.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SidebarItemComponent } from './components/layouts/sidebar-item/sidebar-item.component'
+import { AppRoutingModule } from './router/app-routing.module';
+import { AppComponent } from './components/app/app.component';
+import { MainContentComponent } from './components/layouts/main-content/main-content.component';
+import { ContentViewComponent } from './components/layouts/content-view/content-view.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientListComponent,
+    MainContentComponent,
+    NavbarComponent,
+    SidebarComponent,
+    MainContentComponent,
+    SidebarItemComponent,
+    ContentViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TextFieldModule,
+    NgxChartsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.sass']
+})
+export class NavbarComponent {
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+  ])
+}
